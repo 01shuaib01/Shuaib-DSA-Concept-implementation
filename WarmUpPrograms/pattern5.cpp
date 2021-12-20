e56c4e882fd0dd3e18e6ed9c8f3a2414bea2f1ae
MOHD SHUAIB <66258933+01shuaib01@users.noreply.github.com>
2021-12-20 23:25:10 +0530

    Create pattern5.cpp

---
+++ WarmUpPrograms/pattern5.cpp
@@ -0,0 +1,29 @@
+/*
+
+     a
+    bb
+   ccc
+  dddd
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int k=97;
+    for(int i=1;i<=4;i++)
+    {
+        for(int j=1;j<=4;j++)
+        {
+            if(j>=(5-i))
+                printf("%c",k);
+            else
+                cout<<" ";
+        }
+        cout<<endl;
+        k++;
+    }
+    return 0;
+}
